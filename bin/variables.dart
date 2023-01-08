bde44b23f48a6359ab4a91c70a397991daa1b3f5
Guest1 <meghakalidas99@gmail.com>
2023-01-08 18:57:33 +0530

    Initial commit

---
+++ bin/variables.dart
@@ -0,0 +1,15 @@
+void main(){
+  String name ="Megha";
+      int age = 23;
+      double mark=8.4;
+      String email = 'meghakalidas@gmail.com';
+      int phone = 9539886362;
+
+      print('mydetails');
+      print("Name : $name");
+      print("Age  : $age");
+      print("Mark : $mark");
+      print("Email: $email");
+      print("Phone: $phone");
+
+}
